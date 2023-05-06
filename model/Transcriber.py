360009c770e40c6bc990f246758e2d45836aa211
TS_Kalum <101044362+Thiwanka-Sandakalum@users.noreply.github.com>
2023-05-06 15:28:09 +0000

    .

---
+++ model/Transcriber.py
@@ -0,0 +1,47 @@
+import os
+
+
+def run_process(output_type):
+    command = f"./main -m models/ggml-tiny.bin -f , output.wav {output_type}  -of /workspaces/Transcriber/output"
+    try:
+        os.system(command)
+    except OSError as error:
+        print(f"Error: {error}")
+
+
+def output(ftype):
+    ftype = int(ftype)
+    if ftype == 1:
+        return "-otxt"
+    elif ftype == 2:
+        return "-ovtt"
+    elif ftype == 3:
+        return "-osrt"
+    elif ftype == 4:
+        return "-olrc"
+    elif ftype == 5:
+        return "-ocsv"
+    elif ftype == 6:
+        return "-oj"
+
+
+def build_command():
+    print("1 : Output result in a text file\n"
+          "2 : Output result in a vtt file\n"
+          "3 : Output result in a srt file\n"
+          "4 : Output result in a lrc file\n"
+          "5 : Output result in a CSV file\n"
+          "6 : Output result in a JSON file\n")
+
+    while True:
+        ftype = input("Please insert file type (1-6): ")
+        if ftype.isdigit() and 1 <= int(ftype) <= 6:
+            output_type = output(ftype)
+            print(output_type)
+            run_process(output_type)
+            break
+        else:
+            print("Invalid input. Please enter a number between 1 and 6.")
+
+
+build_command()
--- pod_down.py
+++ pod_down.py
@@ -2,55 +2,79 @@ import re
 import requests
 from bs4 import BeautifulSoup
 from tqdm import tqdm
+from converter import convert
+import os
 
 class PodcastDownloader:
     def __init__(self, url):
         self.url = url
         self.episodes = {}
         self.soup = None
+        self.get_soup()
 
     def get_soup(self):
-        r = requests.get(self.url)
-        self.soup = BeautifulSoup(r.content, 'html.parser')
+        try:
+            r = requests.get(self.url)
+            r.raise_for_status()  # Raise an exception if the request was not successful
+            self.soup = BeautifulSoup(r.content, 'html.parser')
+            self.get_episodes()
+        except requests.exceptions.RequestException as e:
+            print(f"Error: Failed to retrieve the webpage - {e}")
+            raise
 
     def get_episodes(self):
-        divs = self.soup.find_all('div', attrs={'class': 'oD3fme'})
-        for ix, div in enumerate(divs[::-1]):
-            date = div.find('div', attrs={'class': 'OTz6ee'}).text
-            name = div.find('div', attrs={'class': 'e3ZUqe'}).text
-            name = re.sub('[\/:*?"<>|]+', '', name)
-            url = div.find('div', attrs={'jsname': 'fvi9Ef'}).get('jsdata').split(';')[1]
-            file_name = f'EP {ix:03d} - {name} ({date})'
-            self.episodes[ix] = {'url': url, 'file_name': file_name}
-            print(file_name)
+        try:
+            divs = self.soup.find_all('div', attrs={'class': 'oD3fme'})
+            for ix, div in enumerate(divs[::-1]):
+                date = div.find('div', attrs={'class': 'OTz6ee'}).text
+                name = div.find('div', attrs={'class': 'e3ZUqe'}).text
+                name = re.sub('[\/:*?"<>|]+', '', name)
+                url = div.find('div', attrs={'jsname': 'fvi9Ef'}).get('jsdata').split(';')[1]
+                file_name = f'EP {ix:03d} - {name} ({date})'
+                self.episodes[ix] = {'url': url, 'file_name': file_name}
+                print(file_name)
+
+            selected_episode = int(input("Enter the episode number you want to download: "))
+            self.download_episode(selected_episode)
+        except Exception as e:
+            print(f"Error: Failed to retrieve the episodes - {e}")
+            raise
 
     def download_episode(self, episode_number):
-        selected_episode = self.episodes[episode_number]
-        url = selected_episode['url']
-        file_name = selected_episode['file_name']
-        podcast = requests.get(url, stream=True)
+        try:
+            selected_episode = self.episodes[episode_number]
+            url = selected_episode['url']
+            file_name = selected_episode['file_name']
+            podcast = requests.get(url, stream=True)
+            podcast.raise_for_status()  # Raise an exception if the download request was not successful
 
-        # get the file size from the content-length header
-        file_size = int(podcast.headers.get('Content-Length', 0))
+            # get the file size from the content-length header
+            file_size = int(podcast.headers.get('Content-Length', 0))
 
-        # create a progress bar using tqdm
-        progress_bar = tqdm(total=file_size, unit='B', unit_scale=True)
+            # create a progress bar using tqdm
+            progress_bar = tqdm(total=file_size, unit='B', unit_scale=True)
 
-        # iterate over the content and write it to the file
-        with open(f'{file_name}.mp3', 'wb') as out:
-            for chunk in podcast.iter_content(chunk_size=1024):
-                if chunk: # filter out keep-alive new chunks
-                    out.write(chunk)
-                    progress_bar.update(len(chunk))
+            # iterate over the content and write it to the file
+            with open(f'{file_name}.mp3', 'wb') as out:
+                for chunk in podcast.iter_content(chunk_size=1024):
+                    if chunk:  # filter out keep-alive new chunks
+                        out.write(chunk)
+                        progress_bar.update(len(chunk))
 
-        # close the progress bar
-        progress_bar.close()
-        return f'{file_name}.mp3'
+            print(f'{file_name} downloaded')
 
-if __name__ == '__main__':
-    url = main.podcast_url
-    podcast = PodcastDownloader(url)
-    podcast.get_soup()
-    podcast.get_episodes()
-    selected_episode = int(input("Enter episode number you want to download: "))
-    podcast.download_episode(selected_episode)
+            # convert .mp3 to .wav
+            print("Converting...")
+            convert(f'{file_name}.mp3')
+
+            # remove downloaded mp3 file
+            os.remove(f'{file_name}.mp3')
+
+            # close the progress bar
+            progress_bar.close()
+
+            return f'{file_name}.wav'
+        except Exception as e:
+            print(f"Error: Failed to download the episode - {e}")
+            raise
+
